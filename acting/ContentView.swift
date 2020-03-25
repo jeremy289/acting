87ec0529739a8b185b34f941ce5a63b26118be7b
as468579 <as468579@gmail.com>
2020-03-25 17:01:32 +0800

    79797

--- acting/ContentView.swift
+++ acting/ContentView.swift
@@ -10,7 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 50) {
+            HStack {
+                Image("牛排2")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150.0, height: 100.0).clipped()
+                
+                Text("牛排")
+            }
+            HStack {
+                Image("胡椒")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150.0, height: 100.0).clipped()
+                Text("胡椒")
+            }
+            HStack {
+                Image("蛇丸")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150.0, height: 100.0)
+                Text("蛇丸")
+            }
+        
+        }
+        
     }
 }
 
---
+++ member.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  acting
+//
+//  Created by User18 on 2020/3/25.
+//  Copyright © 2020 ntou. All rights reserved.
+//
+
+import Foundation
+struct member{
+    let name: String
+}
